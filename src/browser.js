3e8f6c5e06e59421bbf28fb78c94680677a22d2e
Léo <toonbunny@gmail.com>
2019-12-15 01:33:50 +0100

    Update browser.js

--- src/browser.js
+++ src/browser.js
@@ -1,31 +1,83 @@
-const core = require('./core');
-
-document.querySelectorAll('[type="text/x-francescript"]').forEach(frToJs);
-
-window.france = core;
-
-function frToJs(scriptNode) {
-  var text = scriptNode.textContent || getText(scriptNode); scriptNode.parentNode.removeChild(scriptNode);
-  addScript(core.compile(text, "fr"));
-}
-
-function getText(node) {
-  var text = node.getAttribute('src');
+ _     _ _   _           
+| |   (_) | | |          
+| |__  _| |_| | ___ _ __ 
+| '_ \| | __| |/ _ \ '__|
+| | | | | |_| |  __/ |   
+|_| |_|_|\__|_|\___|_|   
   
-  if (text !== null && text.length) {
-    var xml = new XMLHttpRequest();
-    
-    xml.open('GET', text, false);
-    xml.send(null);
-    
-    if (xml.status == 200 || xml.status == 0) {
-    		return xml.responseText;
-    	}
-   }
-}
-
-function addScript(content) {
-  var js = document.createElement("script");
-  js.innerHTML = content;
-  document.body.appendChild(js);
-}
+  
+  
+  
+  
+                                                 _    
+                                                           /M\        
+                                                          O   O 
+                                                          |/ \     
+                                                          \___|
+                                                         / ___/       
+                                                         |   /      
+                                                         /   /       
+                                                         /   /        
+                                                        /    /        
+                                                        /    /        
+                                                        /    /        
+    \                                                   /   /      
+    \\                                                  /   /     
+     \|                                                 /   /         
+     | |                                                |   |         
+     \ |                                                |   |        
+     \ \                                               /    |        
+     \  |                                              |    /        
+      \ \                                              /    |       
+      \  \                                             |   /         
+      \  \                                            |    /         
+      \  \                                           |     /         
+      \  \                                           /    |/          
+      /  \                                           /    |           
+      /  \                                           /    /           
+      /  \                                           |    /           
+     /   /                                          /    |/           
+     |   /                                          /    |            
+    |    |                                          /    /            
+    |    |                                         |     /            
+   |     |                                         /    |/            
+  |     |                                          /    |             
+ |     /                                           /    /             
+/     |                                           /     /             
+     |                                           |     //             
+    \                                          //      |              
+    \                                        -/        /            
+    \                                      -/          /              
+    \                                     /           |/              
+    \                     ________/---/--/            /             
+|   \               -----/                           |/__           
+|   |          /---/                                 |   \           
+\    \        /                                     |/   |            
+\    |       -                                      /     \          
+ \   \      /                                      /       |          
+ |    |    -                        \             /        |         
+ \    \   /                         |        \             |          
+ |    \   |                         \         \            \         
+ \     \ /     \           \         |         |           |          
+  |    \ /     \           |         \         \        |  \         
+  |     \|     \            |         |         |       /   |         
+   \     \      \           |         |         |       /   |         
+   |      \     /\          |         \         \      |    |         
+   |       \   /  \          \         |         |    /     |         
+    \          /   |         |\\\\\\\\\\\       |     /     \        
+     \         /    \\       |           \       |/--/ \     |       
+      \\      /\      \      |            |      |     |     |        
+        \     / \     /\      \           |      |     \     |        
+         \    / /    / |      |           \      \     |     |       
+          \--/  /    / \     /            |      |     \     |       
+               /     /  \     |           \      \     \     |       
+               |    |    |    |            |     |     |     \       
+               |    |    |    |            |     |     \     |       
+                |   \    \    |            \     /     |     /       
+                |   /    |    \            |     |     \     |        
+                /   |     \   |            \     \      \    /       
+                |   \     |    |            |    |      \    |        
+                /    |    |    \            |    |      \    |        
+                |    \    |    |            /    \       |    \     
+                /   #\    /     \           /    |       /    |       
+                          /  ###\          /  ###\       /  ##\  
